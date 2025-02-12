ace2b530d898f7a1e0870b29c7b4d30e1776ca53
Walter Moreira <walter@waltermoreira.net>
2025-02-11 18:27:33 -0600

    Add flake to run the template generator

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        app = pkgs.writeShellApplication {
+          name = "copier";
+          runtimeInputs = with pkgs; [
+            copier
+            git
+          ];
+          text = ''
+            copier copy gh:waltermoreira/lean4-template .
+          '';
+        };
+      in
+      {
+        apps.default = {
+          type = "app";
+          program = "${app}/bin/copier";
+        };
+      }
+    );
+}
